c21b46846258fd9bd8e2cd63e4a400ea23cb03ac
Julian Radovanov <jradovanov@gmail.com>
2023-04-14 18:13:34 +0300

    third lesson
    Start of third lesson

---
+++ third_lesson_python_basic.py
@@ -0,0 +1,11 @@
+'''
+Will start with Python rules for names we have to use.
+As we mentioned in second lesson, it is important to use names,
+which will tell to the readers what the name it tings do.
+Usually, one program had been written from many programmers
+Everyone have to understand from the name what it is about.
+Other rule connected to names is "snake_case".
+This mean to write only with lower letters and to use "_" between the words
+
+
+'''
