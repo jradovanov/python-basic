8a4b93874855eb82a801a371f312586392d9d4a5
Julian Radovanov <jradovanov@gmail.com>
2023-04-16 20:55:54 +0300

    Some updates
    Updates in third lesson

--- third_lesson_python_basic.py
+++ third_lesson_python_basic.py
@@ -7,5 +7,18 @@ Everyone have to understand from the name what it is about.
 Other rule connected to names is "snake_case".
 This mean to write only with lower letters and to use "_" between the words
 
+So, after we find out what the function "print" do, it's time to understand what
+kind of data we are working with.
+All data we work for is called "string". This including letters, words, numbers,
+fractional numbers. If we not change them they will always been a string.
+If the data came outside, this happened usually when we solve tasks
+in the university. Then we just write "input()" which mean: I expected strings to
+come from outside. All the data that will come in this way have to be used as a
+string.
+If we want to work with numbers or fractional numbers, then we have to use:
+"int" - for numbers or "float" - for fractional numbers.
+The syntax will be: "int(input())" for numbers, or 'float(input())" for fractional
+numbers.
+These commands will change data in the wanted format.
 
 '''
