655d101fd9e0f709decf919729d7ec689f760ae4
jradovanov <jradovanov@gmail.com>
2023-04-26 17:02:16 +0300

    Update fourth_lesson_python_basic.py

--- fourth_lesson_python_basic.py
+++ fourth_lesson_python_basic.py
@@ -1,4 +1,23 @@
 '''
 This is our fourth lesson.
+Let's do something with our knowledge so far.
+One of the first task is to print the numbers from 1 to 10.
+With all that we know, the only way is to say "print" and corresponding number.
+Example:
+    print(1)
+    print(2)
+    print(3)
+Of course, this is not the only way to print numbers, even this is not a way at all but for now...
 
+Here is the moment to introduce the 'variable"
+The variable is the place where we store data. As the name suggest, the stored data can be modified.
+Let see one example:
+
+a = 5
+print(a)
+a = 6
+print(a)
+
+In this example first we assign a value to the variable and then modify it.
+Python allow this. In many other language this is not possible.
 '''
