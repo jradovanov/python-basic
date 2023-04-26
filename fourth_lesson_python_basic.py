b942d7cc68b376334897be16552b690315b705e1
jradovanov <jradovanov@gmail.com>
2023-04-26 17:46:59 +0300

    Update fourth_lesson_python_basic.py

--- fourth_lesson_python_basic.py
+++ fourth_lesson_python_basic.py
@@ -20,4 +20,23 @@ print(a)
 
 In this example first we assign a value to the variable and then modify it.
 Python allow this. In many other language this is not possible.
+
+How this can help us in this case where have to print numbers from 1 to 10?
+If the condition is like this, the answer is - not at all.
+But, if we expect numbers to be handed one after the others, then we can use the variable. Let see how.
+First we will say to computer that our variable expecting a number.
+Then we want this number to be printed. All this will look like this:
+
+a = int(input())
+print(a)
+
+First we remember that have to use int when we expect a number
+Then, first row say - I am expecting a number. When it comme, store it in the variable "a"
+After that print it.
+
+If the given number is 7, then 7 will be printed. If after that the given number is 2, 2 will be printed.
+What is the use?
+In first example, we have to write print() 10 times. When the variable is used, this will be only this
+two rows because every time the variable "a" will be modified.
+
 '''
