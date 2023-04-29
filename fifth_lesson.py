74db60b6e6ca5a00963a8ed4bbbdcfc145d40efd
Julian Radovanov <jradovanov@gmail.com>
2023-04-29 22:08:44 +0300

    Update fifth_lesson.py

--- fifth_lesson.py
+++ fifth_lesson.py
@@ -1,4 +1,10 @@
 '''
 This is our fifth lesson
+In last lesson we found the variables.
+They have "name", "type" and "value"
+name - have to choose a properly one
+type - "int", "float", "str"
+value - a number, a text, a picture, data, list...
+In Python the value defined the type
 
 '''
