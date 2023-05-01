6afc14932c9ad213285b7cb87d7311ea22a6172d
Julian Radovanov <jradovanov@gmail.com>
2023-05-01 20:10:00 +0300

    Update fifth_lesson.py

--- fifth_lesson.py
+++ fifth_lesson.py
@@ -6,5 +6,11 @@ name - have to choose a properly one
 type - "int", "float", "str"
 value - a number, a text, a picture, data, list...
 In Python the value defined the type
-
+Let's solve a problem for example:
+How much is 5 x 6?
+Let's make three variables:
+a = 5
+b = 6
+c = a x b
+print(c)
 '''
