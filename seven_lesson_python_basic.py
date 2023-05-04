3d49e51d383fe5a56835c70ff254bd5b53a12613
Julian Radovanov <jradovanov@gmail.com>
2023-05-05 00:09:01 +0300

    Update seven_lesson_python_basic.py

--- seven_lesson_python_basic.py
+++ seven_lesson_python_basic.py
@@ -1,14 +1,4 @@
 '''
-We are in seven lesson
-numbers = [int(x) for x in input().split()]
-count = int(input())
 
-for _ in range(count):
-    # number_to_remove = min(numbers)
-    min_num = float("inf")
-    for el in numbers:
-        if el < min_num:
-            min_num = el
-    print(min_num)
 
 '''
