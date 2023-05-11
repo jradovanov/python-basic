9dcf4f23ab3ffdd7fc974ca652c69a84a4140188
jradovanov <jradovanov@gmail.com>
2023-05-11 15:00:41 +0300

    Update eight_lesson_python_basic.py

--- eight_lesson_python_basic.py
+++ eight_lesson_python_basic.py
@@ -10,7 +10,12 @@ So, if we want to check "a"is equals or not to "b" have to write "=="
 How to check:
 First have to use "if" statement followed by condition a == b and ":" for the end.
 The whole expression will be: if a == b:
-
+After that we have to point out what happened if they are equal.
+if a == b:
+    print("Yes!")
+As you can see, "print("Yes!")" is one tab inward.
+This mean that the entire expression that is inside refer to this "if check"
+In Python, when we wants to refer something have to be 4 spaces or 1 tab inward.
 
 
 '''
