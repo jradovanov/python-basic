4bc621c6d01e0c75ecdf15131699825dc6b840d4
Julian Radovanov <jradovanov@gmail.com>
2023-05-12 23:35:17 +0300

    Update eight_lesson_python_basic.py

--- eight_lesson_python_basic.py
+++ eight_lesson_python_basic.py
@@ -19,3 +19,42 @@ In Python, when we wants to refer something have to be 4 spaces or 1 tab inward.
 
 
 '''
+my_set = {1, 2, 3}
+my_set.remove(2)
+print(my_set)
+
+numbers = input().split()
+for index in range(len(numbers) -1, -1, -1):
+    print(numbers[index], end=" ")
+
+
+# Fashion
+
+# clothes = [int(x) for x in input().split()]
+# clothes = input().split()
+# rack_capacity = int(input())
+#
+# used_racks = 1
+# current_rack = rack_capacity
+#
+# while clothes:
+#     current_piece = int(clothes[-1])
+#
+#     if current_piece <= current_rack:
+#         clothes.pop()
+#         current_rack -= current_piece
+#     else:
+#         used_racks += 1
+#         current_rack = rack_capacity
+#
+# print(used_racks)
+
+# from collections import deque
+#
+# kids = deque(input().split())
+# n = int(input())
+#
+# while len(kids) > 1:
+#     kids.rotate(-n)
+#     print(f"Removed {kids.pop()}")
+# print(f"Last is {kids.popleft()}")
