41d7de3bfc628d9a1b3d59ba7ece73eeb523b2bd
Julian Radovanov <jradovanov@gmail.com>
2023-05-14 15:56:21 +0300

    Update eight_lesson_python_basic.py

--- eight_lesson_python_basic.py
+++ eight_lesson_python_basic.py
@@ -19,4 +19,84 @@ In Python, when we wants to refer something have to be 4 spaces or 1 tab inward.
 
 
 '''
+# Reverse Numbers
+
+# numbers = input().split()
+# for index in range(len(numbers) -1, -1, -1):
+#     print(numbers[index], end=" ")
+
+
+
+# Fashion
+
+# clothes = [int(x) for x in input().split()]
+# clothes = input().split()
+# rack_capacity = int(input())
+#
+# used_racks = 1
+# current_rack = rack_capacity
+#
+# while clothes:
+#     current_piece = int(clothes[-1])
+#
+#     if current_piece <= current_rack:
+#         clothes.pop()
+#         current_rack -= current_piece
+#     else:
+#         used_racks += 1
+#         current_rack = rack_capacity
+#
+# print(used_racks)
+
+# from collections import deque
+#
+# kids = deque(input().split())
+# n = int(input())
+#
+# while len(kids) > 1:
+#     kids.rotate(-n)
+#     print(f"Removed {kids.pop()}")
+# print(f"Last is {kids.popleft()}")
+
+
+# task 3
+
+
+
+# task 2
+
+# phrase = input()
+# my_list = []
+#
+# for index in range(len(phrase)):
+#     if phrase[index] == "(":
+#         my_list.append(index)
+#     elif phrase[index] == ")":
+#         start_index = my_list.pop()
+#         print(phrase[start_index:index + 1])
+
+
+# task 1
+# text = list(input())
+# for element in range(len(text)-1, -1, -1):
+#     print(text.pop())
+#     print(type(text))
+# while text:
+#     print(text.pop())
+# print(text)
+
+# def a():
+#     return 5
+#
+#
+# def b():
+#     return 6
+#
+#
+# def c():
+#     print(a())
+#     print(b())
+#
+#
+# c()
 
