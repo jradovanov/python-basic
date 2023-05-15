b7f39b5a79e2214c3c58194b5dec08880e183e0b
jradovanov <jradovanov@gmail.com>
2023-05-15 19:06:33 +0300

    Update eight_lesson_python_basic.py

--- eight_lesson_python_basic.py
+++ eight_lesson_python_basic.py
@@ -6,10 +6,11 @@ This construction checks if a logical conditions is valid for a given expression
 Examples for such conditions are:
 Equals - in Python equals is expressed with "=="
 If we write "=" it means that it assign the value.
-So, if we want to check "a"is equals or not to "b" have to write "=="
+So, if we want to check "a" is equals or not to "b" have to write "=="
 How to check:
 First have to use "if" statement followed by condition a == b and ":" for the end.
 The whole expression will be: if a == b:
+After that, with one "tab" 
 
 
 
